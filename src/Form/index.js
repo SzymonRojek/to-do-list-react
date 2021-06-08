be1b1d6120d7b1e81cba5c1c9e6edfb6bb839130
Szymon Rojek <sz.rojek@gmail.com>
2021-06-08 17:26:27 +0100

    add focus inline to input - ref

--- src/Form/index.js
+++ src/Form/index.js
@@ -10,7 +10,7 @@ const Form = ({ addNewTask }) => {
     if(!newTaskContent) {
       return;
     }
-    
+
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
   }
@@ -21,13 +21,16 @@ const Form = ({ addNewTask }) => {
       onSubmit={onFormSubmit}
     >
       <input 
+        ref={(input) => {input && input.focus() }}    
         value={newTaskContent}
         className="form__input" 
         placeholder="What you have to do?" 
         autoFocus 
         onChange={ ({ target }) => setNewTaskContent(target.value)}
       />
-      <button className="form__button">Add task</button>
+      <button className="form__button">
+        Add task
+      </button>
     </form>
   ); 
 }
