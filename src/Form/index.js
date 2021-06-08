76ad5558ee9d2fb908ec8920c4d3cb7be30d365d
Szymon Rojek <sz.rojek@gmail.com>
2021-06-08 18:07:01 +0100

    edit Form component add useRef, handleFocus

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,8 +1,14 @@
-import { useState } from "react";
+import { useState, useRef  } from "react";
 import "./style.css";
 
 const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
+
+  const textInput = useRef();
+
+  const handleFocus = () => {
+    textInput.current.focus();
+  };
 
   const onFormSubmit = event => {
     event.preventDefault();
@@ -13,22 +19,24 @@ const Form = ({ addNewTask }) => {
 
     addNewTask(newTaskContent.trim());
     setNewTaskContent("");
-  }
+  };
 
   return (
     <form 
       className="form"
       onSubmit={onFormSubmit}
     >
-      <input 
-        ref={(input) => {input && input.focus() }}    
+      <input  
+        ref={textInput}
         value={newTaskContent}
         className="form__input" 
         placeholder="What you have to do?" 
         autoFocus 
         onChange={ ({ target }) => setNewTaskContent(target.value)}
       />
-      <button className="form__button">
+      <button 
+      onClick={handleFocus}
+      className="form__button">
         Add task
       </button>
     </form>
