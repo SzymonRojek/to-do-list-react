a8d5a3b0415c86775eaa2a04df41a6daa8c28ee3
Szymon Rojek <sz.rojek@gmail.com>
2021-06-18 16:05:12 +0100

    add styled components in the Section component - edit all files

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,13 +1,18 @@
-import './style.css';
+import { 
+  SectionWrapper, 
+  StyledHeader, 
+  StyledHeading, 
+  BodyWrapper 
+} from './styled';
 
 export const Section = ({ title, body, extraHeaderContent }) => (
-  <section className="section">
-    <header className="section__header">
-      <h2 className="section__title">{title}</h2>
+  <SectionWrapper>
+    <StyledHeader>
+      <StyledHeading>{title}</StyledHeading>
       {extraHeaderContent}
-    </header>
-    <div className="section__body">
+    </StyledHeader>
+    <BodyWrapper>
       {body}
-    </div>
-  </section>  
+    </BodyWrapper>
+  </SectionWrapper>  
 );
