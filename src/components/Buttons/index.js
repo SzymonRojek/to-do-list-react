1377a1dff297d0be0fcf9f1edee0fcee29fbd6d1
Szymon Rojek <sz.rojek@gmail.com>
2021-06-18 15:47:09 +0100

    add styled components in the Buttons component - edit all files

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,24 +1,22 @@
-import './style.css';
+import { ButtonsWrapper, StyledButton } from './styled';
 
 export const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-    <div className="buttons">
-      {tasks.length > 0 && (
-        <>
-          <button 
-            onClick={toggleHideDone}
-            className="buttons__button"
-            disabled={tasks.every( ({ done }) => !done)} 
-          >
-            {hideDone ? 'Show' : 'Hide'} done
-          </button>
-          <button 
-            onClick={setAllDone}
-            className="buttons__button" 
-            disabled={tasks.every( ({ done }) => done)} 
-          >
-            Mark all done
-          </button>
-        </> 
-      )} 
-    </div>
+  <ButtonsWrapper>
+    {tasks.length > 0 && (
+      <>
+        <StyledButton 
+          onClick={toggleHideDone}
+          disabled={tasks.every( ({ done }) => !done)} 
+        >
+          {hideDone ? 'Show' : 'Hide'} done
+        </StyledButton>
+        <StyledButton 
+          onClick={setAllDone} 
+          disabled={tasks.every( ({ done }) => done)} 
+        >
+          Mark all done
+        </StyledButton>
+      </> 
+    )} 
+  </ButtonsWrapper>
 );
---
+++ src/components/Buttons/styled.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const StyledButton = styled.button`
+  margin: 0 20px 0 0;
+  color: teal;
+  border: none;
+  background-color: transparent;
+  outline: none;
+  cursor: pointer;
+  transition: color .2s ease-in;
+
+  &:hover {
+    color: hsl(180, 100%, 40%);
+  }
+
+  &:active {
+    color: hsl(180, 100%, 15%);
+  }
+
+  &:disabled {
+    background: rgb(255, 255, 255);
+    color: rgb(201, 194, 194);
+    cursor: not-allowed;
+  }
+
+  @media(max-width: 767px) {
+    flex-basis: 100%;
+    margin: 10px;
+  }
+`;
