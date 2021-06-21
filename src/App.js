c3d01b42bdbdf579e39c66194d4b5b6dd95da1c1
Szymon Rojek <sz.rojek@gmail.com>
2021-06-21 11:42:31 +0100

    create useLocalStorageState and useTasks hooks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Container } from './components/Container';
 import { Header } from './components/Header';
 import { Section } from './components/Section';
 import { Form } from './components/Form';
 import { Tasks } from './components/Tasks';
 import { Buttons } from './components/Buttons';
+import { useTasks } from './useTasks';
 
 export function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tasks")) || []
-  );
 
   const toggleHideDone = () => {
     setHideDone(hideDone => !hideDone);
   };
 
-  const removeTask = id => {
-    setTasks(tasks => tasks.filter(task => task.id !== id));
-  };
-
-  const toggleTaskDone = id => {
-    setTasks(tasks => tasks.map(task => {
-      if(task.id === id) {
-        return {...task, done: !task.done};
-      }
-
-      return task;
-    }));
-  };
-
-  const setAllDone = () => {
-    setTasks(tasks => tasks.map(task => ({
-      ...task, 
-      done: true,
-    })));
-  };
-
-  const addNewTask = newTaskContent => {
-    setTasks(tasks => [
-      ...tasks, 
-      {
-        content: newTaskContent,
-        done: false,
-        id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-      }]);
-  };
-
-  useEffect(() => {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-  }, [tasks]);
+  const {
+    tasks,
+    removeTask,
+    toggleTaskDone,
+    setAllDone,
+    addNewTask
+  } = useTasks();
 
   return (
     <Container> 
---
+++ src/useLocalStorageState.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+export const useLocalStorageState = (keyName, initialValue) => {
+  const getInitialValue = () => {
+    const localStorageValue = localStorage.getItem(keyName);
+  
+    if(localStorageValue === null) {
+      return initialValue;
+    } 
+  
+    return JSON.parse(localStorage.getItem(keyName));
+  }
+
+  const [state, setState] = useState(getInitialValue);
+
+  useEffect(() => {
+  localStorage.setItem(keyName, JSON.stringify(state));
+  }, [keyName, state]);
+
+  return [state, setState];
+};
