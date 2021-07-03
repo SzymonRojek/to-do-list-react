996cc0ef66db598a6effba2c3ec145b762cd6099
Szymon Rojek <sz.rojek@gmail.com>
2021-07-03 19:15:43 +0100

    formatting code in the component Tasks index.js

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -11,19 +11,20 @@ export const Tasks = ({ tasks, hideDone, removeTask, toggleTaskDone }) => (
       <Item
         key={task.id}
         hidden={task.done && hideDone}
-        >
-        <Button 
-          toggleDone 
-          onClick={()=> toggleTaskDone(task.id)}
-        >
-          {task.done ? "✓" : ""}
-        </Button>
-          <Content done={task.done}>{task.content}</Content>
-        <Button
-          remove  onClick={()=>removeTask(task.id)}
-        >
-          🗑
-        </Button >
+      >
+      <Button 
+        toggleDone 
+        onClick={()=> toggleTaskDone(task.id)}
+      >
+        {task.done ? "✓" : ""}
+      </Button>
+      <Content done={task.done}>{task.content}</Content>
+      <Button
+        remove  
+        onClick={()=>removeTask(task.id)}
+      >
+        🗑
+      </Button >
       </Item>
     ))}
   </List>
