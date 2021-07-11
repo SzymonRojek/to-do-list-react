ae4bd88320e8a08080605883b3f55a220523c935
Szymon Rojek <sz.rojek@gmail.com>
2021-07-11 14:54:06 +0100

    remove as a from the external link, add target blank

--- src/common/Link/index.js
+++ src/common/Link/index.js
@@ -4,7 +4,7 @@ export const Link = ({ to, children, ...props }) => {
   if (!to) {
     return <span {...props}>{children}</span>
   } else if (/^https?:\/\//.test(to)) {
-    return <StyledExternalLink as="a" href={to} {...props}>{children}</StyledExternalLink>;
+    return <StyledExternalLink target="_blank" href={to} {...props}>{children}</StyledExternalLink>;
   } else { 
   return <StyledInternalLink to={to} {...props}>{children}</StyledInternalLink>;
   };
